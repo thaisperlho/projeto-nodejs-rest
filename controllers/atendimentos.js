66a95ae689645a17248dbe7fef81127de71d9f56
thaisperlho <56940730+thaisperlho@users.noreply.github.com>
2021-06-30 16:38:09 -0300

    Atualizando REST

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,31 @@
+const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimento e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    })
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+    })
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body)
-        res.send('Você está na rota de atendimento e está realizando um POST') 
+        const atendimento = req.body
+        Atendimento.adiciona(atendimento, res)
+    })
+
+    app.patch('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+        const valores = req.body
+        
+        Atendimento.altera(id, valores, res)
+    })
+
+    app.delete('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.deleta(id, res)
     })
 }  
